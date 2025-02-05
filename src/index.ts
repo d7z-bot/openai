36050a9a1b1611783f46675de474a6aed6d6115e
dragon <ExplodingFKL@gmail.com>
2025-02-05 17:35:24 +0800

    初始化项目

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,54 @@
-import { Context, Schema } from 'koishi'
+import {Awaitable, Context, Schema, Service} from 'koishi'
 
 export const name = 'openai'
 
-export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Retouch {
+  prompt: string
+  replace: string
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export interface Config {
+  url: string
+  token: string
+  model: string
+  retouch: Retouch
+}
+
+export const Config: Schema<Config> = Schema.object({
+  url: Schema.string().description('API地址').default('http://localhost:11434/v1'),
+  token: Schema.string().description('认证 Token'),
+  model: Schema.string().description('模型名称').default('gemma2:9b'),
+  retouch: Schema.object({
+    prompt: Schema.string().description('提示词').role('textarea', { rows: [2, 4] }).default("以猫娘的形式回复"),
+    replace: Schema.string().description('替换内容,仅在需要时使用').default(''),
+  }).description("润色配置")
+})
+
+export function apply(ctx: Context, config: Config) {
+  ctx.plugin(AITools,config)
+}
+
+export class AITools extends Service {
+
+  constructor(ctx: Context,config: Config) {
+    super(ctx, 'ai')
+    ctx.logger.info(JSON.stringify(config))
+  }
+
+  protected start(): Awaitable<void> {
+    this.ctx.logger.info('Starting...')
+    return super.start();
+  }
+
+  protected stop(): Awaitable<void> {
+    this.ctx.logger.info('Stopping...')
+    return super.stop();
+
+  }
+}
+declare module 'koishi' {
+  interface Context {
+    ai: AITools
+  }
 }
